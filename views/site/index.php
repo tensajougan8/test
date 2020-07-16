fc046c4c30327f8ef2c0e69a669434cdac6b78c7
tensajougan8 <52632725+tensajougan8@users.noreply.github.com>
2020-07-16 15:24:10 +0530

    branch

--- views/site/index.php
+++ views/site/index.php
@@ -14,6 +14,14 @@ $this->title = 'My Yii Application';
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
 
+    <div class="jumbotron">
+        <h1>Congratulations!</h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
     <div class="body-content">
 
         <div class="row">
